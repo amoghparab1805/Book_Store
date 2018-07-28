9f9da0c32de380fa28b1e82d3cedb2b4a804781d
amoghparab247 <mumbai.amoghparab@gmail.com>
2018-07-29 00:34:00 +0530

    unicode

--- books/urls.py
+++ books/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'books/add/$', views.BookCreate.as_view(), name='book-add'),
+    url(r'books/edit/(?P<pk>\d+)/$', views.BookUpdate.as_view(), name='edit'),
     url(r'^delete/(?P<pk>\d+)/$', views.BookDelete.as_view(), name='delete')
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from .models import Book
 from django.views.generic.edit import CreateView
 from django.urls import reverse
+from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 from django.shortcuts import get_object_or_404
 
@@ -20,13 +21,16 @@ class DetailView(generic.DetailView):
     model = Book
     template_name = 'books/detail.html'
 
+class BookUpdate(UpdateView):
+    model = Book
+    template_name = 'books/edit.html'
+
+    def get_success_url(self):
+        return reverse('books:index')
+
 class BookDelete(DeleteView):
     model = Book
     template_name = 'books/delete.html'
 
     def get_success_url(self):
-        return reverse('books:index', kwargs={'pk': self.pk})
-
-    def objectDelete(request, book_id):
-        book = get_object_or_404(Book, id=book_id)
-        book.delete()
+        return reverse('books:index')
